977a7164186525b5535982a3f57bc7f2e907774f
Sarath Sasi <sarathukumbalath@outlook.com>
2015-08-27 12:23:19 +0530

    Sample Project To create Di Graph in Xamarin Monotuch
    Sample Project To create Di-Graph in Xamarin Monotuch

---
+++ iOS/Circle.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+
+using Foundation;
+using UIKit;
+using ObjCRuntime;
+
+namespace CircleGraph.iOS
+{
+	public partial class Circle : UIViewController
+	{
+		public Circle () : base ("Circle", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			int _bigCircleLineWidth = 20;
+			int _smallCircleLineWidth = 10;
+
+
+			DashboardGraphView _graphView = new DashboardGraphView (mainCircleView1.Bounds, _bigCircleLineWidth, 0.8f);
+			DashboardGraphView _MgraphView = new DashboardGraphView (mainCircleView1.Bounds, _bigCircleLineWidth, 0.8f);
+
+			DashboardGraphView _graphView1 = new DashboardGraphView (circle1.Bounds, _smallCircleLineWidth, 0.6f);
+			DashboardGraphView _graphView2 = new DashboardGraphView (circle1.Bounds, _smallCircleLineWidth, 0.6f);
+			DashboardGraphView _graphView3 = new DashboardGraphView (circle1.Bounds, _smallCircleLineWidth, 0.6f);
+
+			DashboardGraphView _graphView4 = new DashboardGraphView (circle1.Bounds, _smallCircleLineWidth, 0.6f);
+			DashboardGraphView _graphView5 = new DashboardGraphView (circle1.Bounds, _smallCircleLineWidth, 0.6f);
+			DashboardGraphView _graphView6 = new DashboardGraphView (circle1.Bounds, _smallCircleLineWidth, 0.6f);
+
+			mainCircleView.AddSubview (_graphView);
+			mainCircleView1.AddSubview (_MgraphView);
+
+			circle1.AddSubview (_graphView1);
+			circle2.AddSubview (_graphView2);
+			circle3.AddSubview (_graphView3);
+			circle4.AddSubview (_graphView4);
+			circle5.AddSubview (_graphView5);
+			circle6.AddSubview (_graphView6);
+
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
---
+++ iOS/Circle.designer.cs
@@ -0,0 +1,82 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace CircleGraph.iOS
+{
+	[Register ("Circle")]
+	partial class Circle
+	{
+		[Outlet]
+		UIKit.UIView circle1 { get; set; }
+
+		[Outlet]
+		UIKit.UIView circle2 { get; set; }
+
+		[Outlet]
+		UIKit.UIView circle3 { get; set; }
+
+		[Outlet]
+		UIKit.UIView circle4 { get; set; }
+
+		[Outlet]
+		UIKit.UIView circle5 { get; set; }
+
+		[Outlet]
+		UIKit.UIView circle6 { get; set; }
+
+		[Outlet]
+		UIKit.UIView mainCircleView { get; set; }
+
+		[Outlet]
+		UIKit.UIView mainCircleView1 { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (circle1 != null) {
+				circle1.Dispose ();
+				circle1 = null;
+			}
+
+			if (circle2 != null) {
+				circle2.Dispose ();
+				circle2 = null;
+			}
+
+			if (circle3 != null) {
+				circle3.Dispose ();
+				circle3 = null;
+			}
+
+			if (circle4 != null) {
+				circle4.Dispose ();
+				circle4 = null;
+			}
+
+			if (circle5 != null) {
+				circle5.Dispose ();
+				circle5 = null;
+			}
+
+			if (circle6 != null) {
+				circle6.Dispose ();
+				circle6 = null;
+			}
+
+			if (mainCircleView != null) {
+				mainCircleView.Dispose ();
+				mainCircleView = null;
+			}
+
+			if (mainCircleView1 != null) {
+				mainCircleView1.Dispose ();
+				mainCircleView1 = null;
+			}
+		}
+	}
+}
